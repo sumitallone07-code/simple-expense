04017daa9bf97e2d0b1e0fcb2d4443980e073d61
sumitallone07-code <sumitallone07@gmail.com>
2026-02-11 02:05:00 +0530

    Initialize Flutter app with HomeScreen widget

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Daily Expense")),
+      body: Center(
+        child: Text(
+          "App setup successful",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
