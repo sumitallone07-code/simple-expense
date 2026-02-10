516f824b4d49a4c6ac6a1b0cbc6e40843d6b0d1c
sumitallone07-code <sumitallone07@gmail.com>
2026-02-11 02:07:20 +0530

    Create Expense class with properties and constructor

---
+++ lib/expense.dart
@@ -0,0 +1,13 @@
+class Expense {
+  final double amount;
+  final String category;
+  final String note;
+  final DateTime date;
+
+  Expense({
+    required this.amount,
+    required this.category,
+    required this.note,
+    required this.date,
+  });
+}
